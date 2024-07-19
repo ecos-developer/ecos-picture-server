a7ed6cc21801f0d4a08a9578c9f0e137aa845864
Johevin Blesstowi <BS22-1@slc.net>
2024-07-19 12:34:58 +0700

    testing CI

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('ECOS Image Server')
+    .setTitle('ECOS Image Server (Development Stage)')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
